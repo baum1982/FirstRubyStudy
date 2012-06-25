8eb9907d99c814da22dbdef19c5704de1437c79a
김재환 <A70108@jaehwan-ui-MacBook-Pro.local>
2012-06-25 18:36:20 +0900

    v

---
+++ sample_1.rb
@@ -0,0 +1,71 @@
+# print "ruby is easy!  ㅋㅋㅋㅋㅋ \n"hello,Ruby!"
+# puts 4+3
+
+a="Ruby"
+b=" is funny."
+c=a+b
+
+
+arr = ["a", "b", "c", "d", "e", "f", "g"]
+
+# puts arr[-5..-2]
+
+
+
+sum = 0
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+list.each { |i| sum = sum +i}
+
+puts sum
+
+
+
+for num in list
+  sum = sum + num
+end
+
+puts sum
+
+
+
+#-----------------------------------------------------
+age = 31
+
+# if age >= 40
+  # puts "대선 출마 가능 "
+# elsif age >=19
+  # puts "투표가능 "
+# else  
+  # puts "투표불가 "
+# end
+
+if age >= 18 then print "aaa 음주가능 aaa" end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
